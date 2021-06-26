9a59c2e2c8a563c5f728d2eb3c70ee3636d5a7db
Saurabh Grewal <saurabhgrewal718@gmail.com>
2021-06-27 00:13:55 +0530

    biceps calculated but still needs improvement

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import poseEstimationModule as pm
 
-cap = cv2.VideoCapture('ai/exercise.mp4')
+cap = cv2.VideoCapture('ai/curls.mp4')
 detector = pm.poseDetector()
 count = 0
 dir = 0
@@ -16,6 +16,38 @@ while True:
     img = detector.findPose(img, False)
     lmList = detector.findPosition(img, False)
     # print(lmList)
+
+    if len(lmList) != 0:
+        # Right Arm
+        angle = detector.findAngle(img, 12, 14, 16)
+        # # Left Arm
+        # angle = detector.findAngle(img, 11, 13, 15,False)
+        per = np.interp(angle, (210, 310), (0, 100))
+        bar = np.interp(angle, (220, 310), (650, 100))
+        # print(angle, per)
+        color = (255, 0, 255)
+        if per == 100:
+            color = (0, 255, 0)
+            if dir == 0:
+                count += 0.5
+                dir = 1
+        if per == 0:
+            color = (0, 255, 0)
+            if dir == 1:
+                count += 0.5
+                dir = 0
+        print(count)
+
+        # Draw Bar
+        cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
+        cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
+        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
+                    color, 4)
+
+        # Draw Curl Count
+        cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
+                    (255, 0, 0), 25)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
